58415c83acd3233c4708f27cc743f5f5873519ba
Sean04 <seanwatt98@gmail.com>
2020-09-28 13:23:05 +1000

    Create IR_Decoder.vhd
    Created vhdl file for IR decoder

---
+++ IR_Decoder.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:22:22 09/28/2020 
+-- Design Name: 
+-- Module Name:    IR_Decoder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity IR_Decoder is
+end IR_Decoder;
+
+architecture Behavioral of IR_Decoder is
+
+begin
+
+
+end Behavioral;
+
