cc65905cee2c455d6e75370ddcb9592cabb04108
Sean04 <seanwatt98@gmail.com>
2020-10-23 14:21:29 +1100

    spi finished and working, cleaning up code

--- VHDL/IR_Decoder.vhd
+++ VHDL/IR_Decoder.vhd
@@ -1,44 +1,26 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
 -- Create Date:    13:22:22 09/28/2020 
 -- Design Name: 
--- Module Name:    IR_Decoder - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+-- Module Name:		IR_Decoder - Behavioral 
+-- Project Name:	Media Controller Box
+-- Course:			COMP3601
+-- Team:			Violet
+-------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity IR_Decoder is
-    port(   clk     : IN    STD_LOGIC;
-            reset   : IN    STD_LOGIC;
-				ir		: IN	STD_LOGIC;
-				nBits_out	: out std_logic_vector(7 downto 0);
-				curstate : out std_logic_vector(6 downto 0);
-            data    : OUT   STD_LOGIC_VECTOR(15 DOWNTO 0);
-            done    : OUT   STD_LOGIC);
+    port(   clk     	: in    STD_LOGIC;
+            reset   	: in    STD_LOGIC;
+			ir			: in	STD_LOGIC;
+            data    	: out   STD_LOGIC_VECTOR(15 DOWNTO 0);
+			done    	: out   STD_LOGIC;
+			
+			-- Debugging Singals
+			nBits_out	: out std_logic_vector(7 downto 0);
+			curstate 	: out std_logic_vector(6 downto 0)
+		);
 end IR_Decoder;
 
 architecture Behavioral of IR_Decoder is
@@ -52,9 +34,9 @@ architecture Behavioral of IR_Decoder is
 	end component;
 	
 	component lshift12 is
-		PORT (w, Clock, resetn		: IN	STD_LOGIC;
-				En 			: IN	STD_LOGIC;
-				Q			: OUT	STD_LOGIC_VECTOR(1 TO 12));
+		port (	w, Clock, resetn	: in	STD_LOGIC;
+				En 					: in	STD_LOGIC;
+				Q					: out	STD_LOGIC_VECTOR(1 TO 12));
 	end component;
 	
 	component up_counter IS
@@ -68,14 +50,14 @@ architecture Behavioral of IR_Decoder is
     signal y    : state;
 
     -- Datapath signals
-    signal EC, RC, EA, w, resetn    : STD_LOGIC; -- Load, enable, write and reset for L-Shift Register A
-    signal ET, RT       : STD_LOGIC; -- Enable and reset for timer
-	 signal nBits : integer;
-	 signal data_buffer		: std_logic_vector(11 downto 0);
+	signal EC, RC, EA, w, resetn    : std_logic; -- Load, enable, write and reset for L-Shift Register A
+    signal ET, RT       			: std_logic; -- Enable and reset for timer
+	signal nBits 					: integer;
+	signal data_buffer				: std_logic_vector(11 downto 0);
     
 	-- Timer signals
-	signal usec_out		: integer;
-	signal msec_out		: integer;
+	signal sig_usec			: integer;
+	signal sig_msec			: integer;
 	
 	signal nBit_counter		: std_logic_vector(7 downto 0);
 		
@@ -100,39 +82,38 @@ begin
                 when S2 =>
                 -- Check the timer
 					-- if timer < 2.4ms
-						if ir = '1' then
-                    if msec_out = 2 and (usec_out < 450 and usec_out > 350) then
-								y <= S3;
-                    else                    -- the ir signal has been 0 for 2.6ms, begin reading a command
-								y <= S1;
-                    end if;
-					   else
-							y <= S2;
+					if ir = '1' then
+						if sig_msec = 2 and (sig_usec < 450 and sig_usec > 350) then
+									y <= S3;
+						else                    -- the ir signal has been 0 for 2.6ms, begin reading a command
+									y <= S1;
 						end if;
+					else
+						y <= S2;
+					end if;
                 
                 -- State 3
                 -- Ready to receive a bit
                 when S3 =>
---							y <= S4;
-						if nBit_counter = "00001100" then
-							y <= S7;
+					if nBit_counter = "00001100" then
+						y <= S7;
+					else
+						if ir = '1' then
+							y <= S3;
 						else
-							if ir = '1' then
-								y <= S3;
-							else
-								y <= S4;
-							end if;
+							y <= S4;
 						end if;
+					end if;
 
                 -- State 4 
                 -- time the 0 ir signal
                 when S4 =>
 					  if ir = '1' then                    -- switch to state 5/6 to stop timer
---							if usec_out = 600 then
-							if msec_out = 0 and usec_out < 750 and usec_out > 450 then
+--							if sig_usec = 600 then
+							if sig_msec = 0 and sig_usec < 750 and sig_usec > 450 then
 								y <= S5;                    -- if 0 for 0.6ms then go to state 5
-							elsif msec_out = 1 and usec_out > 50 and usec_out < 350 then
---							elsif msec_out = 1 and usec_out = 200 then
+							elsif sig_msec = 1 and sig_usec > 50 and sig_usec < 350 then
+--							elsif sig_msec = 1 and sig_usec = 200 then
 								y <= S6;                    -- if 0 for 1.2ms then go to state 6
 							else 
 								y <= S1;
@@ -150,7 +131,7 @@ begin
 					y <= S3;
 					
 				when S7 =>
-					if msec_out = 30 then
+					if sig_msec = 30 then
 						y <= S1;
 					else
 						y <= S7;
@@ -215,7 +196,7 @@ begin
     end process;
 
     Counter: Timer
-            port map (clk, ET, RT, usec_out, msec_out);
+            port map (clk, ET, RT, sig_usec, sig_msec);
 				
 	 ShiftReg: lshift12
 				port map ( w, clk, resetn, EA, data_buffer(11 downto 0));
